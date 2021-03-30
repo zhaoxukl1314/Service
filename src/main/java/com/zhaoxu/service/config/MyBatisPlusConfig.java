4a1788492e3896a78981bbf9010e0d53dc5ca450
zhaoxukl1314 <zhaoxuk13kl>
2021-03-30 21:49:22 +0800

    init

--- src/main/java/com/zhaoxu/service/config/MyBatisPlusConfig.java
+++ src/main/java/com/zhaoxu/service/config/MyBatisPlusConfig.java
@@ -2,8 +2,10 @@ package com.zhaoxu.service.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
